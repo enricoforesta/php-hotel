5368040345fc85f33fed79adf6adc4473f90704f
enricoforesta <enrico.foresta97@gmail.com>
2024-01-09 15:41:59 +0100

    add: CDN bootstrap

--- index.php
+++ index.php
@@ -46,6 +46,9 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- / BOOTSTRAP -->
     <title>PHP H0TEL</title>
 </head>
 
